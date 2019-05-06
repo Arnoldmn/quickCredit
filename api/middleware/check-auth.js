fb9782a766d120eaf933cff626fdc5cce91fc9cf
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-05-06 15:52:39 +0300

    fix bug on check auth

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
