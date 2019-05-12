5ade14952f33322a23c08936baaaada8cabe7413
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-05-12 18:45:37 +0300

    fix small errors

--- api/server.js
+++ api/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 import userRoute from "./routes/users";
 import loanRoute from "./routes/loan";
+import swaggerDocument from "./routes/doc/swagger.json";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
+const PORT = process.env.PORT || 7000;
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 7000;
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(userRoute);
 app.use(loanRoute);
-//app.use(swaggerRoute);
-app.use(json());
 
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
